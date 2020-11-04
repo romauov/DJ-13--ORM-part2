794d052fbc623647b1fb9488559c74f2c928ae8a
Sergei Romanov <l2omanov@ya.ru>
2020-11-05 02:45:20 +0400

    fix added

--- articles/admin.py
+++ articles/admin.py
@@ -1,31 +1,37 @@
 from django.contrib import admin
-from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+from .models import Article, MainSection, Section
 
-from articles.models import Article, Scope, Section
 
+class MainSectionInlineFormset(BaseInlineFormSet):
+    def check_errors(self):
+        if self.is_main_counter > 1:
+            raise ValidationError('Основным может быть только один раздел.')
+        if self.is_main_counter == 0:
+            raise ValidationError('Укажите основной раздел')
 
-class ScopeInline(admin.TabularInline):
-    model = Scope
-
-class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        self.is_main_counter = 0
         for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-            form.cleaned_data
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            raise ValidationError('Тут всегда ошибка')
-        return super().clean()  # вызываем базовый код переопределяемого метода
+            form_data = form.cleaned_data
+            if form_data and form_data['is_main']:
+                self.is_main_counter += 1
+        self.check_errors()
+        return super().clean()
+
+
+class MainSectionInline(admin.TabularInline):
+    model = MainSection
+    formset = MainSectionInlineFormset
+    extra = 1
+
+
+@admin.register(Article)
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [MainSectionInline]
 
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
-    inlines = [ScopeInline]
-    formset = RelationshipInlineFormset
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    inlines = [ScopeInline]
+    list_display = ['tag_name']
--- articles/models.py
+++ articles/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-class Section(models.Model):
 
-    tag_name = models.TextField(max_length=32, verbose_name='Название раздела')
-    is_main = models.BooleanField(default=False, verbose_name='Основной')
-
-    class Meta:
-        verbose_name = 'Раздел'
-        verbose_name_plural = 'Разделы'
-
-    def __str__(self):
-        return self.tag_name
 
 class Article(models.Model): #Group
 
@@ -18,11 +8,6 @@ class Article(models.Model): #Group
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    sections = models.ManyToManyField(
-        Section,
-        through='scope',
-        through_fields=('article', 'section'),
-    )
 
     class Meta:
         verbose_name = 'Статья'
@@ -31,6 +16,25 @@ class Article(models.Model): #Group
     def __str__(self):
         return self.title
 
-class Scope(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+class Section(models.Model):
+
+    tag_name = models.TextField(max_length=32, verbose_name='Название раздела')
+    articles = models.ManyToManyField(Article, through='MainSection', related_name='sections')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['tag_name']
+
+    def __str__(self):
+        return self.tag_name
+
+
+class MainSection(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.DO_NOTHING)
+    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING, related_name='section')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        ordering = ['section']
